8b9afb6e2276cf45e8c65d1f35bbe778150acd9e
Dimitri <maraindimitri@gmail.com>
2026-01-23 23:40:55 +0100

    Draw map in function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use macroquad::prelude::*;
 
+
+const CASE_SIZE: i32 = 64;
+const MAP_WIDTH: i32 = 7;
+const MAP_HEIGHT: i32 = 7;
+const WINDOW_WIDTH: i32 = 1920;
+const WINDOW_HEIGHT: i32 = 1080;
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Raycaster".to_owned(),
@@ -13,14 +20,33 @@ fn window_conf() -> Conf {
     }
 }
 
+fn draw_map(map: [i32; 49], map_offset_x: i32, map_offset_y: i32){
+    for (i, elem) in map.iter().enumerate() {
+        let curr_col = (i as i32) % MAP_WIDTH;
+        let curr_row = (i as i32) / MAP_WIDTH;
+
+        if *elem == 1 {
+            draw_rectangle(
+                (curr_col * CASE_SIZE + map_offset_x) as f32, 
+                (curr_row * CASE_SIZE + map_offset_y) as f32, 
+                CASE_SIZE as f32, 
+                CASE_SIZE as f32, 
+                RED
+            );
+        }else if *elem == 0 {
+            draw_rectangle(
+                (curr_col * CASE_SIZE + map_offset_x) as f32,
+                (curr_row * CASE_SIZE + map_offset_y) as f32, 
+                CASE_SIZE as f32, 
+                CASE_SIZE as f32, 
+                BLACK
+            );
+        }
+    }
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
-    const CASE_SIZE: i32 = 64;
-    const MAP_WIDTH: i32 = 7;
-    const MAP_HEIGHT: i32 = 7;
-    const WINDOW_WIDTH: i32 = 1920;
-    const WINDOW_HEIGHT: i32 = 1080;
-
     let grid_width = MAP_WIDTH * CASE_SIZE;
     let grid_height = MAP_HEIGHT * CASE_SIZE;
     let map_offset_x = (WINDOW_WIDTH - grid_width) / 2;
@@ -37,34 +63,10 @@ async fn main() {
         1, 1, 1, 1, 1, 1, 1  
     ];
 
-    let mut curr_row: i32;
-    let mut curr_col: i32;
-
     loop{
         clear_background(LIGHTGRAY);
 
-        for (i, elem) in map.iter().enumerate() {
-            curr_col = (i as i32) % MAP_WIDTH;
-            curr_row = (i as i32) / MAP_WIDTH;
-
-            if *elem == 1 {
-                draw_rectangle(
-                    (curr_col * CASE_SIZE + map_offset_x) as f32, 
-                    (curr_row * CASE_SIZE + map_offset_y) as f32, 
-                    CASE_SIZE as f32, 
-                    CASE_SIZE as f32, 
-                    RED
-                );
-            }else if *elem == 0 {
-                draw_rectangle(
-                    (curr_col * CASE_SIZE + map_offset_x) as f32,
-                    (curr_row * CASE_SIZE + map_offset_y) as f32, 
-                    CASE_SIZE as f32, 
-                    CASE_SIZE as f32, 
-                    BLACK
-                );
-            }
-        }
+        draw_map(map, map_offset_x, map_offset_y);
 
         next_frame().await
     }
